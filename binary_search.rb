3db2260cce0c040170ae0cbb91f412dc91813d39
Yee-Ning <yee.ningc@gmail.com>
2013-02-22 22:17:30 -0500

    Tidying up the code

--- binary_search.rb
+++ binary_search.rb
@@ -6,7 +6,7 @@ class Array
 		
 		if low > high
 			puts "Value not found."		
-		elsif (val == self[mid])
+		elsif val == self[mid]
 			puts "Found #{val} in position #{mid}."
 		elsif val < self[mid]
 			binary_search(val, low, mid - 1)	
@@ -20,7 +20,7 @@ end
 arr = (1..1_000_000).to_a
 
 Benchmark.bm(7) do |r|
-	r.report("array:") { arr.binary_search(18, 0, 1_000_000) }
+	r.report("array:") { arr.binary_search(18) }
 end
 
 
